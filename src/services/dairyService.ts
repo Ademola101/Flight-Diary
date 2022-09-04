20d3fe5126608521211a5b8ca6a60554aeaad427
Ademola <ademolaogunmokun@yahoo.com>
2022-09-04 14:42:19 +0100

    resolve json module

---
+++ src/services/dairyService.ts
@@ -0,0 +1,16 @@
+import diaryData from  '../../data/dairies.json';
+
+const getEntries = () => {
+  return diaryData;
+};
+
+
+const addEntry = () => {
+  return null;
+};
+
+
+export default {
+  getEntries,
+  addEntry
+};
