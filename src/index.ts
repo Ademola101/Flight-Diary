68fb28fb9856f35eb88d09d168cb6ae67be01579
Ademola <ademolaogunmokun@yahoo.com>
2022-09-04 14:07:03 +0100

    source file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import dairyRouter from './routes/diaries';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use('/api/diaries', dairyRouter);
 
-const PORT = 3000;
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged me');
   res.send('Hello world');
---
+++ src/routes/diaries.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  res.send('fetching all diaries');
+});
+
+
+router.post('/', (_req, res) => {
+  res.send('saving a diary');
+});
+
+
+export default router;
